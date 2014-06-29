15276ed731c338c8c05aadf14ef5a6d5681fc2ed
Paco van der Linden <pavadeli@gmail.com>
2014-06-30 00:35:19 +0200

    Integrated CanvasTag into solution, including weighted tags.

--- html/components/tagcloud/tagcloud.js
+++ html/components/tagcloud/tagcloud.js
@@ -1,10 +1,39 @@
 angular.module('tagcloud', [])
-.directive('tagCloud', function () {
+.constant('uniqueCanvasId', {
+  current: 0,
+  next: function next() {
+    return this.current++;
+  }
+})
+.directive('tagCloud', function (uniqueCanvasId, $timeout) {
   return {
     restrict: 'E',
     scope: {
       tags: '='
     },
-    templateUrl: 'components/tagcloud/tagcloud.html'
-  }
+    templateUrl: 'components/tagcloud/tagcloud.html',
+    link: function link(scope) {
+      var canvasId = scope.canvasId = 'canvas' + uniqueCanvasId.next(),
+          started = false;
+
+      scope.$watchCollection('tags', function () {
+        if (started) {
+          $timeout(function () {
+            TagCanvas.Update(canvasId);
+          });
+        } else {
+          $timeout(function () {
+            TagCanvas.Start(canvasId, null, {
+              textColour: 'red',
+              weight: true,
+              weightFrom: 'data-weight',
+              weightMode: 'size',
+              weightSize: 0.6
+            });
+          });
+          started = true;
+        }
+      });
+    }
+  };
 });
